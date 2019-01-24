94f757b126e744ead459c696a0111092319e62dc
iotwebplanet <iotwebplanet.bvm@gmail.com>
2019-01-24 14:59:36 +0530

    Create pir-demo.ino
    Sample code for PIR motion sensor

---
+++ pir-demo.ino
@@ -0,0 +1,36 @@
+/*
+All components availble at iotwebplanet.com
+*/
+ 
+int buzzer = 13;                // choose the pin for the LED
+int sensor = 3;               // choose the sensor input pin (for PIR sensor)
+int sensorState = LOW;             // default assuming no motion detected
+int val = 0;                    // variable for reading the sensor data
+ 
+void setup() {
+  pinMode(13, OUTPUT);      // declare LED as output
+  pinMode(sensor, INPUT);     // declare sensor as input
+ 
+  Serial.begin(9600);   //for debug
+}
+ 
+void loop(){
+  val = digitalRead(sensor);  // read sensor input value
+  if (val == HIGH) {            // check if the sensor is HIGH
+    digitalWrite(buzzer, HIGH);  // turn Buzzer/light ON
+    if (sensorState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
+      sensorState = HIGH;
+    }
+  } else {
+    digitalWrite(buzzer, LOW); // turn LED OFF
+    if (sensorState == HIGH){
+      // we have just turned of
+      Serial.println("Motion ended!");
+      // We only want to print on the output change, not state
+      sensorState = LOW;
+    }
+  }
+}
